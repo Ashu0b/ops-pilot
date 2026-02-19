a4b25fef04de3ca52aa2a4b2847fbca1573ebe29
Ashis-Thorium <ashisbisoi05@gmail.com>
2026-02-19 14:30:20 +0530

    change the cors config

--- OPS_PILOT_API/app.py
+++ OPS_PILOT_API/app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask_migrate import Migrate
 from dotenv import load_dotenv
+from flask_cors import CORS
 from core.database import init_db, db
 from router import register_routers
 import models
@@ -13,6 +14,11 @@ def create_app():
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    CORS(
+        app,
+        resources={r"/*": {"origins": os.getenv("FRONTEND_URL")}},
+        supports_credentials=True
+    )
     init_db(app)
     Migrate(app, db)
     register_routers(app)
